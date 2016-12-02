f70d984687c44bdd0f12ff7fbcfba31f323de283
Quite Quiet <femtontusen@gmail.com>
2016-12-02 12:06:55 +0100

    Improvements?

--- 2016/advent2/bin/advent2.dart
+++ 2016/advent2/bin/advent2.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 main() async {
   Map<int, List<int>> dir = {68:[1,0],76:[0,-1],82:[0,1],85:[-1,0]};
   List<List<String>> pad2 = [['-','-','1','-','-'],['-','2','3','4','-'],['5','6','7','8','9'],['-','A','B','C','-'],['-','-','D','-','-']];
@@ -8,15 +9,13 @@ main() async {
   .then((List<String> file) => file.forEach((String line) {
     line.runes.forEach((int c) {
       // Part 1
-      locP1[0] += (locP1[0] + dir[c][0] < 0 || locP1[0] + dir[c][0] > 2 ? 0 : dir[c][0]);
-      locP1[1] += (locP1[1] + dir[c][1] < 0 || locP1[1] + dir[c][1] > 2 ? 0 : dir[c][1]);
+      locP1[0] = max(0, min(locP1[0] + dir[c][0], 2));
+      locP1[1] = max(0, min(locP1[1] + dir[c][1], 2));
       // Part 2
-      int x = locP2[0] + dir[c][0], y = locP2[1] + dir[c][1];
-      x = x < 0 ? 0 : x > 4 ? 4 : x;
-      y = y < 0 ? 0 : y > 4 ? 4 : y;
+      int x = max(0, min(locP2[0] + dir[c][0], 2)),
+          y = max(0, min(locP2[1] + dir[c][1], 2));
       if (pad2[x][y] != '-') {
-        locP2[0] = x;
-        locP2[1] = y;
+        locP2 = [x, y];
       }
     });
     keysP1.add('${locP1[0] * 3 + locP1[1] + 1}');
