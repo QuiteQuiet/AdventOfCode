f244331f8057a3cc2adf89956ca9218673933de3
Quite Quiet <femtontusen@gmail.com>
2016-12-03 09:45:33 +0100

    Just for fun, one-liner solution for d3p1

--- 2016/advent3/bin/advent3.dart
+++ 2016/advent3/bin/advent3.dart
@@ -5,7 +5,7 @@ main() async {
   List<List<int>> triangles = new List();
   int vertical = 0;
   await new File('input.txt').readAsLines()
-  .then((List<String> file) => triangles = file.map((s) => s = s.split(' ').where((s2) => s2.length > 0).toList().map(int.parse).toList()).toList());
+  .then((List<String> file) => triangles = file.map((s) => s.split(' ').where((s2) => s2.length > 0).toList().map(int.parse).toList()).toList());
   for (int i = 0; i < triangles.length; i += 3) {
     vertical += validTriangle([triangles[i][0], triangles[i + 1][0], triangles[i + 2][0]]) ? 1 : 0;
     vertical += validTriangle([triangles[i][1], triangles[i + 1][1], triangles[i + 2][1]]) ? 1 : 0;
@@ -14,3 +14,5 @@ main() async {
   print('Part 1: ${triangles.where(validTriangle).toList().length}');
   print('Part 2: $vertical');
 }
+// Just because I can :)
+//main() => print('Part1:${new File('input.txt').readAsLinesSync().map((s)=>s.split(' ').where((s2)=>s2.length>0).toList().map(int.parse).toList()..sort()).toList().where((t)=>t[0]+t[1]>t[2]).toList().length}');
