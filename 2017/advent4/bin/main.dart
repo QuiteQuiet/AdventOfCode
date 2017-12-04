845eb80f764b7154bdaefad583aaffe8e5ff4c24
Quite Quiet <femtontusen@gmail.com>
2017-12-04 14:18:14 +0100

    Even better day 4 2017 solution

--- 2017/advent4/bin/main.dart
+++ 2017/advent4/bin/main.dart
@@ -3,20 +3,14 @@ import 'dart:io';
 main() async {
   int valid1 = 0, valid2 = 0;
   RegExp bounds = new RegExp(r'(\b[^\s]+?\b).*?\b\1\b');
+  Stopwatch time = new Stopwatch()..start();
   await new File('advent4/input.txt').readAsLines()
   .then((List<String> file) => file.forEach((String line) {
     // part 1 
     if (!bounds.hasMatch(line)) valid1++;
     // part 2
-    List<String> words = line.split(' ').map((e) => (e.split('')..sort()).join()).toList()..sort();
-    bool valid = true;
-    for (String word in words) {
-      if (words.where((e) => word == e).length > 1) {
-        valid = false;
-        break;
-      }
-    }
-    if (valid) valid2++;
+    List<String> words = line.split(' ').map((e) => (e.split('')..sort()).join());
+    if (words.length == new Set.from(words).length) valid2++;
   }));
   print('Part 1: $valid1');
   print('Part 2: $valid2');
