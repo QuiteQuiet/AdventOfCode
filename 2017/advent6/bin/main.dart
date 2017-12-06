621e693abc59f82d91f4deae43d12dd07ac38075
Quite Quiet <femtontusen@gmail.com>
2017-12-06 06:33:02 +0100

    style

--- 2017/advent6/bin/main.dart
+++ 2017/advent6/bin/main.dart
@@ -2,18 +2,16 @@ import 'dart:io';
 import 'dart:math';
 
 main() async {
-  int seen1 = 0;
+  int seen = 0;
   List<int> state = new List<int>();
   Map<String, int> visited = new Map<String, int>();
   await new File('advent6/input.txt').readAsLines()
-  .then((List<String> file) {
-    state = file[0].split('\t').map(int.parse).toList();
-  });
+  .then((List<String> file) =>
+    state = file[0].split('\t').map(int.parse).toList()
+  );
   while (visited[state.toString()] == null) {
-    visited[state.toString()] = seen1;
-    seen1++;
-    int consume = state.reduce(max),
-    index = state.indexOf(consume);
+    visited[state.toString()] = seen++;
+    int consume = state.reduce(max), index = state.indexOf(consume);
     state[index] = 0;
     while (consume > 0) {
       index = (index + 1) % state.length;
@@ -21,6 +19,6 @@ main() async {
       consume--;
     }
   }
-  print('Part 1: $seen1');
-  print('Part 2: ${seen1 - visited[state.toString()]}');
+  print('Part 1: $seen');
+  print('Part 2: ${seen - visited[state.toString()]}');
 }
