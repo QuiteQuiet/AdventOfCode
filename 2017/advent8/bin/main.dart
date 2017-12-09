6b31a36248438d18f16dff1ac0c351cf4a715501
Quite Quiet <femtontusen@gmail.com>
2017-12-09 07:28:39 +0100

    cleaner day 8

--- 2017/advent8/bin/main.dart
+++ 2017/advent8/bin/main.dart
@@ -2,27 +2,30 @@ import 'dart:io';
 import 'dart:math';
 
 main() async {
+  Map<String, Function> compare = {
+    '>': (a, b) => a > b,
+    '<': (a, b) => a < b,
+    '<=': (a, b) => a <= b,
+    '>=': (a, b) => a >= b,
+    '==': (a, b) => a == b,
+    '!=': (a, b) => a != b
+  };
   Map<String, int> registers = new Map<String, int>();
   int inProcessMax = 0;
   await new File('advent8/input.txt').readAsLines()
   .then((List<String> file) => file.forEach((String line) {
     List<String> current = line.split(' ');
-    String target = current[0], condition = current[4];
+    String target = current[0], conditional = current[4], op = current[5];
     if (!registers.containsKey(target)) registers[target] = 0;
-    if (!registers.containsKey(condition)) registers[condition] = 0;
+    if (!registers.containsKey(conditional)) registers[conditional] = 0;
 
     int value = int.parse(current[2]);
-    switch(current[5]) {
-      case '>': if (registers[condition] <= int.parse(current[6])) { return; } break;
-      case '<': if (registers[condition] >= int.parse(current[6])) { return; } break;
-      case '<=': if (registers[condition] > int.parse(current[6])) { return; } break;
-      case '>=': if (registers[condition] < int.parse(current[6])) { return; } break;
-      case '==': if (registers[condition] != int.parse(current[6])) { return; } break;
-      case '!=': if (registers[condition] == int.parse(current[6])) { return; } break;
+    if (compare[op](registers[conditional], int.parse(current[6]))) {
+      registers[target] += current[1] == 'inc' ? value : -value;
+      if (registers[target] > inProcessMax) {
+        inProcessMax = registers[target];
+      }
     }
-    registers[target] += current[1] == 'inc' ? value : -value;
-    int tempMax = registers.values.reduce(max);
-    if (inProcessMax < tempMax) inProcessMax = tempMax;
   }));
   print('Part 1: ${registers.values.reduce(max)}');
   print('Part 2: $inProcessMax');
