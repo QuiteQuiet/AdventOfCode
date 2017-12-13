8c7377d7e6d983c61a1aa744efc8db84594f5d3b
Quite Quiet <femtontusen@gmail.com>
2017-12-13 11:16:49 +0100

    Day 13 2017 solution

---
+++ 2017/advent13/bin/main.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+class Layer {
+  int depth, range;
+  Layer({this.depth : 0, this.range: 0, Iterable<int> data: null}) {
+    if (data.length > 0) {
+      this.depth = data.first;
+      this.range = data.last;
+    }
+  }
+  int pos(int time) => (this.depth + time) % ((this.range - 1) * 2);
+}
+
+main() async {
+  List<Layer> firewall = new List<Layer>();
+  int severity = 0, i;
+  bool gotCaught = true;
+  new File('advent13/input.txt').readAsLines()
+  .then((List<String> file) {
+    firewall = file.map((String line) => new Layer(data: line.split(': ').map(int.parse))).toList();
+    for (i = 0; gotCaught; i++) {
+      gotCaught = false;
+      for (Layer next in firewall) {
+        // entering this layer
+        if (next.pos(i) == 0) {
+          gotCaught = true;
+          if (i == 0) severity += next.depth * next.range;
+          else break;           
+        }
+      }
+    }
+    print('Part 1: $severity');
+    print('Part 2: ${i - 1}');
+  }); 
+}
