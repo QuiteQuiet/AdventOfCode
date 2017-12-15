6134f34b06763e7eb3d1cfcaed81359c42a4a36f
Quite Quiet <femtontusen@gmail.com>
2017-12-15 07:27:21 +0100

    Day 15 2017 solution

---
+++ 2017/advent15/bin/main.dart
@@ -0,0 +1,38 @@
+class Generator {
+  int start, seed, prev, mod = 1;
+  Generator(this.start, this.seed) { this.prev = this.start; }
+  void reset() => this.prev = this.start;
+  String next() {
+    do {
+      this.prev = (this.prev * this.seed) % 2147483647;
+    } while (this.prev % this.mod != 0);
+    return this.prev.toRadixString(2).padLeft(32, '0');
+  }
+}
+
+void main() {
+  // input:
+  // Generator A starts with 783
+  // Generator B starts with 325
+  Generator a = new Generator(783, 16807),
+  b = new Generator(325, 48271);
+  int pairs1 = 0, pairs2 = 0;
+
+  // part 1
+  for (int i = 0; i < 40000000; i++) {
+    String nexta = a.next().substring(16);
+    String nextb = b.next().substring(16);
+    if (nexta == nextb) pairs1++;
+  }
+  
+  // part 2
+  a..reset()..mod = 4;
+  b..reset()..mod = 8;
+  for (int i = 0; i < 5000000; i++) {
+    String nexta = a.next().substring(16);
+    String nextb = b.next().substring(16);
+    if (nexta == nextb) pairs2++;
+  }
+  print('Part 1: $pairs1');
+  print('Part 2: $pairs2');
+}
