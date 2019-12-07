65ef456584049aaab3de4744385e0f3fe39cbcc5
Quite Quiet <femtontusen@gmail.com>
2019-12-07 23:29:10 +0100

    Add 2018:05

---
+++ 2018/advent5/bin/main.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+import 'dart:collection';
+
+String process(String input) {
+  ListQueue<String> stack = ListQueue();
+  input.split('').forEach((c) {
+    if (stack.isEmpty) {
+      stack.add(c);
+    } else if ((c.toLowerCase() == c && c.toUpperCase() == stack.last) ||
+               (c.toUpperCase() == c && c.toLowerCase() == stack.last)) {
+      stack.removeLast();
+    } else {
+      stack.add(c);
+    }
+  });
+  return stack.toList().join('');
+}
+
+void main() {
+  String input = File('input.txt').readAsStringSync();
+  String output = process(input);
+  print('Part 1: ${output.length}');
+
+  String shortest;
+  'abcdefghijklmnopqrstuvwxyz'.split('').forEach((c) {
+    String test = process(output.replaceAll(RegExp(c, caseSensitive: false), ''));
+    shortest ??= test;
+    if (test.length < shortest.length) {
+      shortest = test;
+    }
+  });
+  print('Part 2: ${shortest.length}');
+}
--- 2019/intcode/computer.dart
+++ 2019/intcode/computer.dart
@@ -14,7 +14,7 @@ class IntcodeComputer {
 
   int resolve(int mode, int value) => mode == 0 ? _program[value] : value;
 
-  int run({int noun = null, int verb = null, List<int> input = null, List<int> output = null, noReset: null}) {
+  int run({int noun = null, int verb = null, List<int> input = null, List<int> output = null}) {
     int temp, opcode;
 
     _program[1] = noun ?? _program[1];
@@ -25,61 +25,36 @@ class IntcodeComputer {
       int m1 = temp ~/ 100 % 10, m2 = temp ~/ 1000 % 10;
       switch(opcode) {
         case 1:
-          int a = resolve(m1, _program[pointer + 1]),
-              b = resolve(m2, _program[pointer + 2]),
-              c = _program[pointer + 3];
-          _program[c] = a + b;
+          _program[_program[pointer + 3]] = resolve(m1, _program[pointer + 1]) + resolve(m2, _program[pointer + 2]);
           pointer += 4;
           break;
         case 2:
-          int a = resolve(m1, _program[pointer + 1]),
-              b = resolve(m2, _program[pointer + 2]),
-              c = _program[pointer + 3];
-          _program[c] = a * b;
+          _program[_program[pointer + 3]] = resolve(m1, _program[pointer + 1]) * resolve(m2, _program[pointer + 2]);
           pointer += 4;
           break;
         case 3:
           if (input.length < 1) {
-            return -1;
+            return null;
           }
           _program[_program[pointer + 1]] = input.removeAt(0);
           pointer += 2;
           break;
         case 4:
-          int a = resolve(m1, _program[pointer + 1]);
-          output.add(a);
+          output.add(resolve(m1, _program[pointer + 1]));
           pointer += 2;
           break;
         case 5:
-          int a = resolve(m1, _program[pointer + 1]),
-              b = resolve(m2, _program[pointer + 2]);
-          if (a != 0) {
-            pointer = b;
-          } else {
-            pointer += 3;
-          }
+          pointer = resolve(m1, _program[pointer + 1]) != 0 ? resolve(m2, _program[pointer + 2]) : pointer + 3;
           break;
         case 6:
-          int a = resolve(m1, _program[pointer + 1]),
-              b = resolve(m2, _program[pointer + 2]);
-          if (a == 0) {
-            pointer = b;
-          } else {
-            pointer += 3;
-          }
+          pointer = resolve(m1, _program[pointer + 1]) == 0 ? resolve(m2, _program[pointer + 2]) : pointer + 3;
           break;
         case 7:
-          int a = resolve(m1, _program[pointer + 1]),
-              b = resolve(m2, _program[pointer + 2]),
-              c = _program[pointer + 3];
-          _program[c] = a < b ? 1 : 0;
+          _program[_program[pointer + 3]] = resolve(m1, _program[pointer + 1]) < resolve(m2, _program[pointer + 2]) ? 1 : 0;
           pointer += 4;
           break;
         case 8:
-          int a = resolve(m1, _program[pointer + 1]),
-              b = resolve(m2, _program[pointer + 2]),
-              c = _program[pointer + 3];
-          _program[c] = a == b ? 1 : 0;
+          _program[_program[pointer + 3]] = resolve(m1, _program[pointer + 1]) == resolve(m2, _program[pointer + 2]) ? 1 : 0;
           pointer += 4;
           break;
         case 99:
