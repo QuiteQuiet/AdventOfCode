051aa36f898408f1f982acf8b4b5d616e35fc492
Quite Quiet <femtontusen@gmail.com>
2019-12-09 21:53:56 +0100

    Stuff

--- 2019/advent9/bin/main.dart
+++ 2019/advent9/bin/main.dart
@@ -2,9 +2,10 @@ import 'dart:io';
 import '../../intcode/computer.dart';
 
 void main() {
+  Stopwatch time = Stopwatch()..start();
   String input = File('input.txt').readAsStringSync();
   IntcodeComputer computer = IntcodeComputer(input);
 
-  print('Part 1: ${(computer..alloc(2000)).run(input: [1], output: [])}');
-  print('Part 2: ${(computer..alloc(2000)).run(input: [2], output: [])}');
+  print('Part 1: ${(computer..alloc(100)).run(input: [1], output: [])} ${time.elapsed}');
+  print('Part 2: ${(computer..alloc(200)).run(input: [2], output: [])} ${time.elapsed}');
 }
