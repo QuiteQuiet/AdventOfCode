4660fb3ecf3c30275b927326a021d5b96355e69a
Quite Quiet <femtontusen@gmail.com>
2019-12-16 22:54:16 +0100

    optimizations

--- 2019/advent16/bin/main.dart
+++ 2019/advent16/bin/main.dart
@@ -1,22 +1,13 @@
 import 'dart:io';
-
-List<int> fft(List<int> signal, List<List<int>> patterns) {
-  List<int> output = [];
-  for (int i = 0; i < signal.length; i++) {
-    int sum = 0;
-    for (int j = i; j < signal.length; j++) {
-        sum += signal[j] * patterns[i][j];
-    }
-    output.add(sum.abs() % 10);
-  }
-  return output;
-}
+import 'dart:collection';
 
 void main() {
+  Stopwatch time = Stopwatch()..start();
   String input = File('input.txt').readAsStringSync();
   List<int> signal = input.split('').map(int.parse).toList();
   List<int> basePattern = [0, 1, 0, -1];
   List<List<int>> patterns = [];
+
   for (int i = 0; i < signal.length; i++) {
     List<int> pattern = []..addAll(List.filled(i + 1, basePattern[0]))
                           ..addAll(List.filled(i + 1, basePattern[1]))
@@ -31,23 +22,31 @@ void main() {
   }
 
   for(int i = 0; i < 100; i++) {
-    signal = fft(signal, patterns);
+    List<int> temp = [];
+    for (int i = 0; i < signal.length; i++) {
+      int sum = 0;
+      for (int j = i; j < signal.length; j++) {
+          sum += signal[j] * patterns[i][j];
+      }
+      temp.add(sum % 10);
+    }
+    temp = signal;
   }
-  print('Part 1: ${signal.sublist(0, 8).join('')}');
+  print('Part 1: ${signal.sublist(0, 8).join('')} ${time.elapsed}');
 
   int offset = int.tryParse(input.substring(0, 7)) ?? -1;
   int jumps = offset ~/ input.length;
   int index = offset % input.length;
   input *= (10000 - jumps);
   input = input.substring(index);
-  List<int> signal2 = input.split('').map(int.parse).toList();
+  Queue<int> signal2 = Queue.from(input.split('').map(int.parse));
   for (int i = 0; i < 100; i++) {
     int sum = 0;
-    List<int> temp = [];
-    for (int el in signal2.reversed) {
-      temp.add((sum += el) % 10);
+    Queue<int> temp = Queue();
+    while (signal2.isNotEmpty) {
+      temp.addFirst((sum += signal2.removeLast()) % 10);
     }
-    signal2 = temp.reversed.toList();
+    signal2 = temp;
   }
-  print('Part 2: ${signal2.sublist(0, 8).join('')}');
+  print('Part 2: ${signal2.take(8).join('')} ${time.elapsed}');
 }
