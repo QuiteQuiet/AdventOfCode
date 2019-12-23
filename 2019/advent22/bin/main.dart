bbe85927089a3e00c46648810fecfb67fb39db37
Quite Quiet <femtontusen@gmail.com>
2019-12-23 10:02:12 +0100

    Add 2019:23

--- 2019/advent22/bin/main.dart
+++ 2019/advent22/bin/main.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-List<int> deal(List<int> deck) => deck.reversed.toList();
 List<int> shift(List<int> deck, int index) {
   if (index < 0) {
     int i = (deck.length + index) % deck.length;
@@ -10,7 +9,7 @@ List<int> shift(List<int> deck, int index) {
     return deck.sublist(i)..addAll(deck.sublist(0, i));
   }
 }
-List<int> spread(List<int> deck, int size) {
+List<int> deal(List<int> deck, int size) {
   List<int> newDeck = List.generate(deck.length, (i) => null);
   int index = 0;
   for (int i = 0; i < deck.length; i++) {
@@ -25,15 +24,17 @@ void main() {
   List<int> deck = List.generate(10007, (i) => i);
   for (String line in input) {
     if (line == 'deal into new stack')
-      deck = deal(deck);
+      deck = deck.reversed.toList();
     else if (line.startsWith('cut'))
       deck = shift(deck, int.parse(line.substring(4)));
     else if (line.startsWith('deal with increment'))
-      deck = spread(deck, int.parse(line.substring(20)));
+      deck = deal(deck, int.parse(line.substring(20)));
   }
   print('Part 1: ${deck.indexOf(2019)}');
 
-  // the "I don't understand math enough for this" part
+  // Credits to https://www.reddit.com/user/mcpower_/ and
+  // https://www.reddit.com/r/adventofcode/comments/ee0rqi/2019_day_22_solutions/fbnkaju/
+  // for this solution.
   BigInt cards = BigInt.from(119315717514047);
   BigInt shuffles = BigInt.from(101741582076661);
   BigInt offsetDiff = BigInt.from(0), incMul = BigInt.from(1);
@@ -41,10 +42,8 @@ void main() {
   BigInt inv(BigInt n) => n.modPow(cards - BigInt.from(2), cards);
   for (String line in input) {
     if (line == 'deal into new stack') {
-      incMul *= BigInt.from(-1);
-      incMul %= cards;
-      offsetDiff += incMul;
-      offsetDiff %= cards;
+      incMul = (incMul * BigInt.from(-1)) % cards;
+      offsetDiff = (offsetDiff + incMul) % cards;
     } else if (line.startsWith('cut')) {
       offsetDiff += incMul * BigInt.from(int.parse(line.substring(4)));
       offsetDiff %= cards;
@@ -55,6 +54,5 @@ void main() {
   }
   BigInt inc = incMul.modPow(shuffles, cards);
   BigInt offset = offsetDiff * (BigInt.from(1) - inc) * inv((BigInt.from(1) - incMul) % cards);
-  BigInt card = (offset + inc * BigInt.from(2020)) % cards;
-  print('Part 2: $card');
+  print('Part 2: ${(offset + inc * BigInt.from(2020)) % cards}');
 }
