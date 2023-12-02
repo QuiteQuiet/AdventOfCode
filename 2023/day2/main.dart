fabdeb63659eb49f2710d83373d5c93c2d5d121b
Quite Quiet <femtontusen@gmail.com>
2023-12-02 14:37:37 +0100

    some cleanup

--- 2023/day2/main.dart
+++ 2023/day2/main.dart
@@ -5,23 +5,22 @@ dynamic convert(String s) => int.tryParse(s) ?? s;
 
 void main() async {
   List<String> lines = await File('input.txt').readAsLines();
-  const int maxRed = 12, maxGreen = 13, maxBlue = 14;
   int possibleSum = 0, powerSum = 0;
   for (String line in lines) {
-    List<List<dynamic>> parts = RegExp(r'(\w+) (\w+)').allMatches(line).map(
-    (e) => [convert(e.group(1)!), convert(e.group(2)!)]).toList();
+    List<List<dynamic>> pulls = RegExp(r'(\w+) (\w+)').allMatches(line).map(
+      (e) => [convert(e.group(1)!), convert(e.group(2)!)]).toList();
 
-    int red = 0, green = 0, blue = 0, game = parts.removeAt(0)[1];
-    parts.forEach((element) => switch (element) {
+    int red = 0, green = 0, blue = 0, game = pulls.removeAt(0)[1];
+    pulls.forEach((element) => switch (element) {
         [int c, 'red'] => red = max(c, red),
         [int c, 'green'] => green = max(c, green),
         [int c, 'blue'] => blue = max(c, blue),
         _ => print,
-      });
-    if (red <= maxRed && green <= maxGreen && blue <= maxBlue) {
+    });
+    if (red <= 12 && green <= 13 && blue <= 14) {
       possibleSum += game;
     }
-    powerSum += red * blue * green;
+    powerSum += red * green * blue;
   }
   print('Part 1: $possibleSum');
   print('Part 2: $powerSum');
