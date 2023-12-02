4f563fe7d6fb62575b19a6366cfb569d01f88cba
Quite Quiet <femtontusen@gmail.com>
2023-12-02 06:58:54 +0100

    Day 2 2023

--- lib/string.dart
+++ lib/string.dart
@@ -2,3 +2,7 @@ extension NumberParsing on String {
   int toInt({Function(String) onError = print }) => int.tryParse(this) ?? onError(this);
   double toDouble({Function(String) onError = print}) => double.tryParse(this) ?? onError(this);
 }
+
+extension Operations on String {
+  String removeLast(int n) => substring(0, length - n);
+}
