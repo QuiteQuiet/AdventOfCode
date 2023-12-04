13bebd5c44b9711505f239740bde304764243a3f
Quite Quiet <femtontusen@gmail.com>
2023-12-04 17:33:54 +0100

    funny optimizations

--- 2023/day4/main.dart
+++ 2023/day4/main.dart
@@ -2,22 +2,24 @@ import 'dart:io';
 
 void main() async {
   List<String> lines = await File('input.txt').readAsLines();
+  Stopwatch time = Stopwatch()..start();
   int points = 0, cards = 0;
   List<int> stack = List.filled(lines.length, 1, growable: true);
-  for (String line in lines) {
+  for (final (int index, String line) in lines.indexed) {
     List<String> everything = line.split(':')[1].split(RegExp(r' +'));
     int divider = everything.indexOf('|');
     Set<String> numbers = Set.from(everything.sublist(0, divider))..remove('');
     Set<String> winningNumbers = Set.from(everything.sublist(divider));
 
-    int cardCount = stack.removeAt(0);
+    int cardCount = stack[index];
     int matches = numbers.intersection(winningNumbers).length;
     for (int i = 0; i < matches; i++) {
-        stack[i] += cardCount;
+        stack[index + i] += cardCount;
     }
     cards += cardCount;
     points += matches > 0 ? 1 << matches - 1 : 0;
   }
   print('Part 1: $points');
   print('Part 2: $cards');
+  print('${time.elapsed}');
 }
