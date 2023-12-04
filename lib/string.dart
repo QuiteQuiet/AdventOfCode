93bfc547f3e9e381aba89c2ebb9c84b740a47a58
Quite Quiet <femtontusen@gmail.com>
2023-12-04 07:13:37 +0100

    2023 day 4

--- lib/string.dart
+++ lib/string.dart
@@ -1,6 +1,10 @@
 extension NumberParsing on String {
   int toInt({Function(String) onError = print }) => int.tryParse(this) ?? onError(this);
   double toDouble({Function(String) onError = print}) => double.tryParse(this) ?? onError(this);
+  bool isDigit() => switch (this) {
+  '0' || '1' || '2' || '3' || '4' || '5' || '6' || '7' || '8' || '9' => true,
+  _ => false,
+};
 }
 
 extension Operations on String {
