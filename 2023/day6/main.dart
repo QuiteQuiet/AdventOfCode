6f2b33963ed4d47c54eb911e911ed4537f8ed06e
Quite Quiet <femtontusen@gmail.com>
2023-12-06 19:07:09 +0100

    tried optimizing but meh

--- 2023/day6/main.dart
+++ 2023/day6/main.dart
@@ -1,26 +1,21 @@
 int simulate(int duration, int record) {
   int wins = 0, speed = 0;
-  while (duration >= 0) {
-    if (duration-- * speed++ > record) {
+  while (duration >= 0)
+    if (duration-- * speed++ > record)
       wins++;
-    }
-  }
   return wins;
 }
 
 void main() async {
   Stopwatch time = Stopwatch()..start();
   List<(int, int)> races = [(38, 241), (94, 1549), (79, 1074), (70, 1091)];
-  List<String> p2Duration = [], p2Record = [];
+  final (int p2d, int p2r) = (38947970, 241154910741091);
 
   int product = 1;
   for (final (duration, record) in races) {
     product *= simulate(duration, record);
-    p2Duration.add(duration.toString());
-    p2Record.add(record.toString());
   }
   print('Part 1: $product');
-  print('Part 2: ${simulate(int.parse(p2Duration.join("")),
-                            int.parse(p2Record.join("")))}');
+  print('Part 2: ${simulate(p2d, p2r)}');
   print('${time.elapsed}');
 }
