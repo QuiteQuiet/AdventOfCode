7636d6b1c0ea3f7c0f282eaec7e7763a246f04dc
Quite Quiet <femtontusen@gmail.com>
2023-12-12 23:09:49 +0100

    2023 day 12

--- 2023/day11/main.dart
+++ 2023/day11/main.dart
@@ -2,27 +2,13 @@ import 'dart:io';
 import 'package:AdventOfCode/grid.dart';
 import "package:trotter/trotter.dart";
 
-int distance(Grid<String> input, List<(int, int)> galaxies, int multiplier) {
-  // Recalculate dimensions
-  List<int> cols = List.generate(input.width, (i) => i)..sort();
-  List<int> rows = List.generate(input.height, (i) => i)..sort();
-  galaxies.forEach((e) {
-    cols.remove(e.$1);
-    rows.remove(e.$2);
-  });
-
-  List<(int, int)> newGalaxies = [];
-  galaxies.forEach((e) {
-    int x = e.$1, y = e.$2;
-    x += cols.where((e) => e < x).length * (multiplier - 1);
-    y += rows.where((e) => e < y).length * (multiplier - 1);
-    newGalaxies.add((x, y));
-  });
-
+int distance(List<(int, int, int, int)> galaxies, int multiplier) {
+  int real(int coord, int blanks) => coord + blanks * (multiplier - 1);
   // Manhattan distance
   int d = 0;
-  for (final List<(int, int)> c in Combinations(2, newGalaxies)()) {
-    d += (c[0].$1 - c[1].$1).abs() + (c[0].$2 - c[1].$2).abs();
+  for (final List<(int, int, int, int)> c in Combinations(2, galaxies)()) {
+    d += (real(c.first.$1, c.first.$3) - real(c.last.$1, c.last.$3)).abs();
+    d += (real(c.first.$2, c.first.$4) - real(c.last.$2, c.last.$4)).abs();
   }
   return d;
 }
@@ -37,7 +23,20 @@ void main() async {
       galaxies.add((x, y));
     }
   });
+  List<int> cols = List.generate(input.width, (i) => i);
+  List<int> rows = List.generate(input.height, (i) => i);
+  galaxies.forEach((e) {
+    cols.remove(e.$1);
+    rows.remove(e.$2);
+  });
 
-  print('Part 1: ${distance(input, galaxies, 2)}');
-  print('Part 2: ${distance(input, galaxies, 1000000)}');
+  List<(int, int, int, int)> newGalaxies = [];
+  galaxies.forEach((g) {
+    int ex = cols.where((e) => e < g.$1).length;
+    int ey = rows.where((e) => e < g.$2).length;
+    newGalaxies.add((g.$1, g.$2, ex, ey));
+  });
+
+  print('Part 1: ${distance(newGalaxies, 2)}');
+  print('Part 2: ${distance(newGalaxies, 1000000)}');
 }
---
+++ 2023/day12/main.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+
+// target is 1738259948652
+
+Function memoizeVariants() {
+  Map<String, int> cache = {};
+  int wrapped(List<String> springs, BigInt groups, bool lava) {
+    String hash = '${springs.join()}:${groups.toRadixString(16)}:$lava';
+    if (cache.containsKey(hash)) {
+      int value = cache[hash]!;
+      return value;
+    }
+
+    if (groups == 0)
+      return cache[hash] = springs.contains('#') ? 0 : 1;
+    else if (springs.isEmpty)
+      return cache[hash] = groups > BigInt.zero ? 0 : 1;
+    else if (groups & BigInt.from(0xF) == BigInt.zero)
+      return cache[hash] = ['?', '.'].contains(springs.first) ?
+        wrapped(springs.sublist(1), groups >> 4, false) : 0;
+    else if (lava)
+      return cache[hash] = ['?', '#'].contains(springs.first) ?
+        wrapped(springs.sublist(1), groups - BigInt.one, true) : 0;
+    else if (springs.first == '#')
+      return cache[hash] = wrapped(springs.sublist(1), groups - BigInt.one, true);
+    else if (springs.first == '.')
+      return cache[hash] = wrapped(springs.sublist(1), groups, false);
+    else
+      return cache[hash] =
+        wrapped(springs.sublist(1), groups, false) +
+        wrapped(springs.sublist(1), groups - BigInt.one, true);
+  }
+  return wrapped;
+}
+
+void main() async {
+  List<String> input = await File('input.txt').readAsLines();
+  Function variants = memoizeVariants();
+
+  int valid1 = 0, valid2 = 0;
+  for (final (int i, String line) in input.indexed) {
+    print('$i: $line');
+
+    List<String> parts = line.split(' ');
+    List<String> springs = parts[0].split('');
+    List<int> numbers = parts[1].split(',').map(int.parse).toList();
+    BigInt groups = BigInt.zero;
+    for (final (int i, int e) in numbers.indexed) {
+      groups |= BigInt.from(e << (i * 4));
+    }
+    valid1 += variants(springs, groups, false) as int;
+
+    springs = [...springs, '?', ...springs, '?', ...springs, '?', ...springs, '?', ...springs];
+    BigInt fiveGroups = BigInt.zero;
+    for (int i = 0; i < 5; i++)
+      fiveGroups |= groups << numbers.length * i * 4;
+    valid2 += variants(springs, fiveGroups, false) as int;
+  }
+  print('Part 1: $valid1');
+  print('Part 2: $valid2');
+}
