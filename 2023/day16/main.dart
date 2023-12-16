9be2c02628dc4a60a681d54a423c853a9d19244a
Quite Quiet <femtontusen@gmail.com>
2023-12-16 07:00:16 +0100

    2023 day 16

---
+++ 2023/day16/main.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+import 'package:AdventOfCode/grid.dart';
+import 'package:AdventOfCode/int.dart';
+
+int energize(Grid<String> mirrors, ({int x, int y, int dx, int dy}) start) {
+  Set<({int x, int y, int dx, int dy})> lights = {};
+  List<({int x, int y, int dx, int dy})> toCheck = [start];
+
+  Set<(int, int)> unique = {};
+  while (toCheck.isNotEmpty) {
+    final pos = toCheck.removeLast();
+    if (pos.x < 0 || pos.x >= mirrors.width ||
+        pos.y < 0 || pos.y >= mirrors.height ||
+        lights.contains(pos)) continue;
+    lights.add(pos);
+    unique.add((pos.x, pos.y));
+
+    switch (mirrors.at(pos.x, pos.y)) {
+      case '/':
+        toCheck.add((x: pos.x - pos.dy, y: pos.y - pos.dx, dx: -pos.dy, dy: -pos.dx));
+      case r'\':
+        toCheck.add((x: pos.x + pos.dy, y: pos.y + pos.dx, dx: pos.dy, dy: pos.dx));
+      case '-':
+        if (pos.dx != 0) continue empty;
+        toCheck.add((x: pos.x - pos.dy, y: pos.y, dx: -pos.dy, dy: 0));
+        toCheck.add((x: pos.x + pos.dy, y: pos.y, dx: pos.dy, dy: 0));
+      case '|':
+        if (pos.dy != 0) continue empty;
+        toCheck.add((x: pos.x, y: pos.y - pos.dx, dx: 0, dy: -pos.dx));
+        toCheck.add((x: pos.x, y: pos.y + pos.dx, dx: 0, dy: pos.dx));
+
+      empty:
+      case '.':
+        toCheck.add((x: pos.x + pos.dx, y: pos.y + pos.dy, dx: pos.dx, dy: pos.dy));
+    }
+  }
+  return unique.length;
+}
+
+void main() async {
+  Grid<String> mirrors = Grid.string(await File('input.txt').readAsString(), (e) => e);
+
+  print('Part 1: ${energize(mirrors, (x: 0, y: 0, dx: 1, dy: 0))}');
+
+  int best = 0;
+  for (int i in 0.to(mirrors.width - 1)) {
+    for ((int, int) d in [(0, 1), (mirrors.height - 1, -1)]) {
+      int test = energize(mirrors, (x: i, y: d.$1, dx: 0, dy: d.$2));
+      if (test > best) {
+        best = test;
+      }
+    }
+  }
+  for (int i in 0.to(mirrors.height - 1)) {
+    for ((int, int) d in [(0, 1), (mirrors.width - 1, -1)]) {
+      int test = energize(mirrors, (x: d.$1, y: i, dx: d.$2, dy: 0));
+      if (test > best) {
+        best = test;
+      }
+    }
+  }
+  print('Part 2: $best');
+}
