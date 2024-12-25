f861874eb1f1ac4e79315f06e1a37b08030a3aba
Quite Quiet <femtontusen@gmail.com>
2024-12-25 12:19:35 +0100

    finish up 2018 day 7

--- 2018/day7/main.dart
+++ 2018/day7/main.dart
@@ -30,4 +30,33 @@ void main() async {
     }
   }
   print('Part 1: ${order.join("")}');
+
+  required = requirements(lines);
+  List<(String, int)> workers = List.filled(5, ('', 0));
+  int seconds = 0;
+  while (required.isNotEmpty) {
+    List<String> left = required.keys.toList()..sort();
+    Iterable<String> toDo = left.where((step) => required[step]!.isEmpty);
+    for (String next in toDo) {
+      for (final (int i, val) in workers.indexed) {
+        final (String _, int remain) = val;
+        if (remain <= 0) {
+          workers[i] = (next, 60 + next.codeUnits[0] - 0x40);
+          required.remove(next);
+          break;
+        }
+      }
+    }
+    for (final (int i, worker) in workers.indexed) {
+      int remain = worker.$2 - 1;
+      if (remain <= 0) {
+        for (Set<String> deps in required.values) {
+          deps.remove(worker.$1);
+        }
+      }
+      workers[i] = (worker.$1, remain);
+    }
+    seconds++;
+  }
+  print('Part 2: ${seconds + workers.where((e) => e.$2 > 0).fold(0, (a, b) => a + b.$2)}');
 }
--- 2024/day25/main.dart
+++ 2024/day25/main.dart
@@ -2,7 +2,6 @@ import 'package:AdventOfCode/aoc_help/get.dart' as aoc;
 
 void main() async {
   String input = (await aoc.getInputString()).replaceAll('#', '1').replaceAll('.', '0').replaceAllMapped(RegExp(r'([01])\n'), (m) => m.group(1)!);
-
   List<int> things = input.split('\n').map((n) => int.parse(n, radix: 2)).toList();
 
   int unique = 0;
