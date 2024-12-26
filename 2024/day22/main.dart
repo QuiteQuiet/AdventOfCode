646813e9602e290cbd7ed182d9b619898c9354be
Quite Quiet <femtontusen@gmail.com>
2024-12-26 09:47:07 +0100

    save 300ms on 2024 day 22

--- 2024/day22/main.dart
+++ 2024/day22/main.dart
@@ -9,33 +9,26 @@ int next(int n) {
 
 void main() async {
   List<int> lines = (await aoc.getInput()).map(int.parse).toList();
-
-  Map<int, int> sequences = {};
+  List<int> sequences = List.generate(0xFFFFF, (index) => 0);
 
   int sum = 0;
   for (int secret in lines) {
-    List<int> buffer = [];
     Set<int> seen = {};
-    int prize = secret % 10;
+    int register = 0, diff = 0, prize = secret % 10;
 
-    for (int i = 0; i < 1999; i++) {
+    for (int i = 0; i < 2000; i++) {
       secret = next(secret);
-      buffer.add((secret % 10) - prize);
+      diff = (secret % 10) - prize;
       prize = secret % 10;
 
-      if (buffer.length >= 4) {
-        int start = buffer.length - 4, change = 0;
-        for (int i = 0; i < 4; i++)
-          change |= (buffer[start + i] + 10) << (20 * (3 - i));
-        if (!seen.contains(change)) {
-          sequences[change] = (sequences[change] ?? 0) + prize;
-          seen.add(change);
-        }
+      register = ((register << 5) | (diff + 9)) & 0xFFFFF;
+      if (i >= 4 && seen.add(register)) {
+        sequences[register] += prize;
       }
     }
     sum += secret;
 
   }
   print('Part 1: $sum');
-  print('Part 2: ${sequences.values.reduce((a, b) => a < b ? b : a)}');
+  print('Part 2: ${sequences.reduce((a, b) => a < b ? b : a)}');
 }
