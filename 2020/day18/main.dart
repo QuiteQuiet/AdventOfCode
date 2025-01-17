732a7767211807faec5e0c7d9fdd4313fb18961e
Quite Quiet <femtontusen@gmail.com>
2025-01-17 19:01:26 +0100

    2020 day 18

---
+++ 2020/day18/main.dart
@@ -0,0 +1,49 @@
+import 'package:AdventOfCode/aoc_help/get.dart' as aoc;
+import 'package:AdventOfCode/string.dart';
+
+int regularMath(List<String> math) {
+  Map<String, int Function(int, int)> operations = {'+': (a, b) => a + b,
+                                                    '*': (a, b) => a * b};
+  int index = 0, res = math[index++].toInt();
+  while (index < math.length)
+    res = operations[math[index++]]!(res, math[index++].toInt());
+  return res;
+}
+
+int advancedMath(List<String> math) {
+  List<int> priority = [];
+  for (int i = 0; i < math.length; i++) {
+    if (math[i] == '+')
+      priority.add(math[++i].toInt() + priority.removeLast().toInt());
+    else if (math[i] != '*')
+      priority.add(math[i].toInt());
+  }
+  return priority.fold(1, (p, e) => p * e);
+}
+
+int helpWithHomework(List<List<String>> homework, int Function(List<String>) howToMath) {
+  int sum = 0;
+  List<List<String>> stack = [];
+  for (List<String> line in homework) {
+    stack.add([]);
+    for (int index = 0; index < line.length; index++) {
+      if (line[index] == '(') {
+        stack.add([]);
+      } else if (line[index] == ')') {
+        List<String> part = stack.removeLast();
+        stack.last.add(howToMath(part).toString());
+      } else {
+        stack.last.add(line[index]);
+      }
+    }
+    sum += howToMath(stack.removeLast());
+  }
+  return sum;
+}
+
+void main() async {
+  List<List<String>> lines = (await aoc.getInput()).map((e) => e.replaceAll(' ', '').split('')).toList();
+
+  print('Part 1: ${helpWithHomework(lines, regularMath)}');
+  print('Part 1: ${helpWithHomework(lines, advancedMath)}');
+}
